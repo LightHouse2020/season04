5574c137f730286eeee44db8bf73e06b3813dfa9
lingzhidong <note_980869266@yahoo.co.jp>
2020-02-21 10:24:50 +0900

    new added

---
+++ com/company/program2.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class program2 {
+
+    public static void main(String[] args) {
+
+        //input a1
+        double x,y;
+        System.out.println("input x of x/y");
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextDouble();
+        System.out.println("input y of x/y");
+        Scanner sr=new Scanner(System.in);
+        y=sc.nextDouble();
+
+        //create a new list
+        double[] List = new double[20];
+        List[0]=x/y;
+
+        //calculate a2~a20
+        recursive(20,x,y,List);
+       //calculate sum and print each element
+        double sum=0.0;
+        for(int i=0;i<20;i++){
+            System.out.println("No."+i+"= "+List[i]+"\n");
+            sum+=List[i];
+        }
+        System.out.println("Sum= "+sum+"\n");
+    }
+    public static void recursive(int n,double x,double y,double List[]){
+        for(int i=1;i<n;i++){
+            double tmp=x;
+            x=x+y;
+            y=tmp;
+            List[i]=x/y;
+        }
+    }
+}
---
+++ com/company/program3.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class program3 {
+    public static void main(String[] args) throws IOException {
+       System.out.println("input an integer x: ");
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("The result is "+factorial(a));
+    }
+    //calculate factorial x!
+    public static double factorial(int n){
+        double b=1;
+        for(int i=1;i<=n;i++){
+            b=b*i;
+        }
+        return b;
+    }
+}
