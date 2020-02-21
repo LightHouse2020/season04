84dd93ea0d994f79b89da583c74621c0f031ad4a
lingzhidong <note_980869266@yahoo.co.jp>
2020-02-21 11:46:05 +0900

    new added

--- 凌志栋/program4.java
+++ 凌志栋/program4.java
@@ -1,9 +1,14 @@
 package 凌志栋;
 
+import java.util.Scanner;
+
 public class program4 {
     public static void main(String[] args){
-
-
-
+        int[] a=new int[5];
+        a[0]=10;
+        for(int i=1;i<=4;i++){
+            a[i]=a[i-1]+2;
+        }
+        System.out.println(a[4]);
     }
 }
