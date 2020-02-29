d03e3f6ca82177480a71059bdce869aba2fd4bd3
MENG <mcr469100216@gmail.com>
2020-02-29 09:52:17 +0900

    第六次作业

---
+++ 孟陈锐/作业6 JAVA面向对象（上）/src/Animal.java
@@ -0,0 +1,15 @@
+
+//继承：这是一个父类
+
+public class Animal {
+	String name;
+	int age=20;
+	
+	public Animal() {
+	}
+	
+	public Animal(String name,int age) {
+		this.name=name;
+		this.age=age;
+	}
+}
---
+++ 孟陈锐/作业6 JAVA面向对象（上）/src/Vehicle.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+//01_交通工具(Vehicle)的类
+//设置初始速度方法，加速方法，减速方法
+
+public class Vehicle {
+	int speed;
+	float size;
+	
+	public Vehicle(int speed,float size) {
+		this.speed = speed;
+		this.size = size;
+	}
+	
+	public void setSpeed(int speed_set) {
+		this.speed = speed_set;
+	}
+	
+	public void speedUp(int up) {
+		this.speed+=up;
+	}
+
+	public void speedDown(int down) {
+		this.speed-=down;
+	}
+	
+	public static void main(String [] args) {
+		Vehicle carVehicle=new Vehicle(100,200);
+		System.out.println("现在的速度："+carVehicle.speed);
+		System.out.println("大小："+carVehicle.size);
+		carVehicle.setSpeed(120);
+		System.out.println("现在的速度："+carVehicle.speed);
+		System.out.println("加速20：");
+		carVehicle.speedUp(20);
+		System.out.println("现在的速度："+carVehicle.speed);
+		System.out.println("减速100：");
+		carVehicle.speedDown(100);
+		System.out.println("现在的速度："+carVehicle.speed);
+	}
+}
